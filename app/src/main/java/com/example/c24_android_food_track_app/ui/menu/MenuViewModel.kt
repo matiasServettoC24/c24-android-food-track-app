f0146b884337364804c94673a7a4ab25c81391df
roman.abaev <roman.abaev@check.de>
2023-05-12 18:16:39 +0200

    remove line

--- app/src/main/java/com/example/c24_android_food_track_app/ui/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/menu/MenuViewModel.kt
@@ -66,7 +66,6 @@ class MenuViewModel : ViewModel() {
                                 )
                             )
                         }
-                        slots.size
                     }
 
                 }
