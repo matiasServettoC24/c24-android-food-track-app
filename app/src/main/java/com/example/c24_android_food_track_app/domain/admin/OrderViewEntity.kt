b32da84f115c65900d7de7e2f9e30b54e71cf904
Haris <m.haris@check24.de>
2023-05-12 17:20:48 +0200

    added submit orders

--- app/src/main/java/com/example/c24_android_food_track_app/domain/admin/OrderViewEntity.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/domain/admin/OrderViewEntity.kt
@@ -5,5 +5,10 @@ import com.example.c24_android_food_track_app.domain.ViewEntity
 data class OrderViewEntity(
     val id: String,
     val title: String,
-    val isReady: Boolean
+    val isReady: Boolean,
+
+    // for DB
+    val status: String,
+    val email: String,
+    val slot: String
 ): ViewEntity
