34493980c59bf2173453b40374e50aa4cd4c4e64
Martin Schwarzboeck <martin.schwarzboeck@check24.de>
2023-05-12 14:55:58 +0200

    first commmit

---
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.c24_android_food_track_app.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
