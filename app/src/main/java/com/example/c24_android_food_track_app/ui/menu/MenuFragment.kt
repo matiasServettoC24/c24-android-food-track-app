36ff861ecdc72ab6af91e376464f5e49cbea9fd4
roman.abaev <roman.abaev@check.de>
2023-05-12 15:15:40 +0200

    cleanup and rename fragment

--- app/src/main/java/com/example/c24_android_food_track_app/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/menu/MenuFragment.kt
@@ -4,34 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.c24_android_food_track_app.databinding.FragmentDashboardBinding
+import androidx.fragment.app.viewModels
+import com.example.c24_android_food_track_app.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentMenuBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    val dashboardViewModel by viewModels<MenuViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel = ViewModelProvider(this)[MenuViewModel::class.java]
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/c24_android_food_track_app/ui/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/menu/MenuViewModel.kt
@@ -1,13 +1,7 @@
 package com.example.c24_android_food_track_app.ui.menu
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MenuViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
 }
