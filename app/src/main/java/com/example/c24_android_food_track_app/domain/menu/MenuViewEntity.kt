56fd59d66a29a2fd6064954e5e600696a98c69a6
roman.abaev <roman.abaev@check.de>
2023-05-12 16:03:47 +0200

    added viewEntity for adapter delegate

---
+++ app/src/main/java/com/example/c24_android_food_track_app/domain/menu/MenuViewEntity.kt
@@ -0,0 +1,8 @@
+package com.example.c24_android_food_track_app.domain.menu
+
+import com.example.c24_android_food_track_app.domain.ViewEntity
+
+data class MenuViewEntity(
+    val dishTitle: String,
+    val dishType: String,
+): ViewEntity
---
+++ app/src/main/java/com/example/c24_android_food_track_app/domain/menu/TimeSlotViewEntity.kt
@@ -0,0 +1,9 @@
+package com.example.c24_android_food_track_app.domain.menu
+
+import com.example.c24_android_food_track_app.domain.ViewEntity
+
+data class TimeSlotViewEntity(
+    val isAvailable: Boolean,
+    val startTime: String,
+    val endTime: String,
+): ViewEntity
