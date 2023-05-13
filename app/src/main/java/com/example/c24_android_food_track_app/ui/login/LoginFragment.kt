9792192093e505c6c63a9d5520fac34d3e7f6a77
Matias Servetto <matias.servetto@check24.de>
2023-05-13 11:12:23 +0200

    Avoid login again

--- app/src/main/java/com/example/c24_android_food_track_app/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/login/LoginFragment.kt
@@ -7,12 +7,16 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.c24_android_food_track_app.R
 import com.example.c24_android_food_track_app.databinding.FragmentLoginBinding
 import com.example.c24_android_food_track_app.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -48,27 +52,31 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             }
         }
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.login.collect {
-                when (it) {
-                    is Resource.Loading -> {
-                        binding.btnLogin.visibility = View.INVISIBLE
-                        binding.loginProgressBar.visibility = View.VISIBLE
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.login.collect {
+                        when (it) {
+                            is Resource.Loading -> {
+                                binding.btnLogin.visibility = View.INVISIBLE
+                                binding.loginProgressBar.visibility = View.VISIBLE
+                            }
+                            is Resource.Success -> {
+                                binding.loginProgressBar.visibility = View.GONE
+                                findNavController().navigate(R.id.action_navigation_home_to_navigation_dashboard)
+                            }
+                            is Resource.Error -> {
+                                Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                                binding.btnLogin.visibility = View.VISIBLE
+                                binding.loginProgressBar.visibility = View.GONE
+                            }
+                            else -> Unit
+                        }
                     }
-                    is Resource.Success -> {
-                        binding.loginProgressBar.visibility = View.GONE
-                        findNavController().navigate(R.id.action_navigation_home_to_navigation_dashboard)
-                    }
-                    is Resource.Error -> {
-                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
-                        binding.btnLogin.visibility = View.VISIBLE
-                        binding.loginProgressBar.visibility = View.GONE
-                    }
-                    else -> Unit
                 }
             }
         }
-
     }
 
 }
--- app/src/main/java/com/example/c24_android_food_track_app/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/login/LoginViewModel.kt
@@ -8,6 +8,7 @@ import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -17,7 +18,7 @@ class LoginViewModel @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ) : ViewModel() {
 
-    private val _login = MutableSharedFlow<Resource<FirebaseUser>>()
+    private val _login = MutableStateFlow<Resource<FirebaseUser>>(Resource.Init())
     val login: Flow<Resource<FirebaseUser>> = _login.asSharedFlow()
 
     fun login(email: String, password: String) {
