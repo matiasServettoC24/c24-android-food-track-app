25e8711c7fa6592cb00981c34bd3e81de2bc3e93
Matias Servetto <matias.servetto@check24.de>
2023-05-27 21:11:55 +0200

    Adds initial previews

--- app/src/main/java/com/example/c24_android_food_track_app/ui/admin/adapters/orderAdapterDelegate.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/admin/adapters/orderAdapterDelegate.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.c24_android_food_track_app.R
+import com.example.c24_android_food_track_app.data.models.FoodTrackOrder
 import com.example.c24_android_food_track_app.data.models.Status
 import com.example.c24_android_food_track_app.databinding.AdminOrderItemComposeBinding
 import com.example.c24_android_food_track_app.domain.ViewEntity
@@ -24,33 +27,73 @@ fun orderAdapterDelegate(
 
     bind {
         binding.root.setContent {
-            MaterialTheme {
-                Row {
-                    Column {
-                        Text(item.order.email)
-                        Text(item.order.title)
-                        item.order.slotTime?.let { Text(it) }
-                    }
-                    when (item.order.status) {
-                        Status.Ordered -> {
-                            Button(onClick = { onOrderReadyCallback(item) }) {
-                                Text("READY")
-                            }
-                        }
-                        Status.Ready -> {
-                            Button(onClick = { onOrderPickedUpCallback(item) }) {
-                                Text("PICKED UP")
-                            }
-                        }
-                        Status.Picked -> {
-                            Button(onClick = { deleteOrderCallback(item) }) {
-                                Text("DELETE")
-                            }
-                        }
-                    }
-
-                }
-            }
+            ItemViewContent(
+                item,
+                onOrderReadyCallback,
+                onOrderPickedUpCallback,
+                deleteOrderCallback
+            )
         }
     }
 }
+
+@Composable
+private fun ItemViewContent(
+    item: OrderViewEntity,
+    onOrderReadyCallback: (OrderViewEntity) -> Unit,
+    onOrderPickedUpCallback: (OrderViewEntity) -> Unit,
+    deleteOrderCallback: (OrderViewEntity) -> Unit,
+) = MaterialTheme {
+    Row {
+        Column {
+            Text(item.order.email)
+            Text(item.order.title)
+            item.order.slotTime?.let { Text(it) }
+        }
+        when (item.order.status) {
+            Status.Ordered -> {
+                Button(onClick = { onOrderReadyCallback(item) }) {
+                    Text("READY")
+                }
+            }
+            Status.Ready -> {
+                Button(onClick = { onOrderPickedUpCallback(item) }) {
+                    Text("PICKED UP")
+                }
+            }
+            Status.Picked -> {
+                Button(onClick = { deleteOrderCallback(item) }) {
+                    Text("DELETE")
+                }
+            }
+        }
+
+    }
+}
+
+private const val previewWidthDp = 300
+
+private val previewFoodTrackOrder = FoodTrackOrder(
+    id = "id",
+    title = "Pizza Margarita",
+    status = Status.Ordered,
+    email = "matias.servetto@check24.de",
+    slot = "10-12",
+    timeStart = "12:00",
+    slotTime = "10:35-10:45"
+)
+
+@Preview(widthDp = previewWidthDp, showBackground = true)
+@Composable
+private fun StatusOrderedPreview() = ItemViewContent(
+    OrderViewEntity(previewFoodTrackOrder.copy(status = Status.Ordered)), {}, {}, {})
+
+@Preview(widthDp = previewWidthDp, showBackground = true)
+@Composable
+private fun StatusReadyPreview() = ItemViewContent(
+    OrderViewEntity(previewFoodTrackOrder.copy(status = Status.Ready)), {}, {}, {})
+
+@Preview(widthDp = previewWidthDp, showBackground = true)
+@Composable
+private fun StatusPickedPreview() = ItemViewContent(
+    OrderViewEntity(previewFoodTrackOrder.copy(status = Status.Picked)), {}, {}, {})
