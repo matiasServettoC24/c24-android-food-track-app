7ae070dd5d62526b8dae58b50e45b87793c1167b
Matias Servetto <matias.servetto@check24.de>
2023-05-27 20:43:30 +0200

    Admin orders migration to simple composable views into adapter delegate

--- app/src/main/java/com/example/c24_android_food_track_app/ui/admin/adapters/orderAdapterDelegate.kt
+++ app/src/main/java/com/example/c24_android_food_track_app/ui/admin/adapters/orderAdapterDelegate.kt
@@ -1,10 +1,15 @@
 package com.example.c24_android_food_track_app.ui.admin.adapters
 
-import androidx.core.view.isVisible
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.example.c24_android_food_track_app.R
-import com.example.c24_android_food_track_app.databinding.AdminOrderItemBinding
-import com.example.c24_android_food_track_app.domain.ViewEntity
 import com.example.c24_android_food_track_app.data.models.Status
+import com.example.c24_android_food_track_app.databinding.AdminOrderItemComposeBinding
+import com.example.c24_android_food_track_app.domain.ViewEntity
 import com.example.c24_android_food_track_app.domain.admin.OrderViewEntity
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegate
 
@@ -12,35 +17,40 @@ fun orderAdapterDelegate(
     onOrderReadyCallback: (OrderViewEntity) -> Unit,
     onOrderPickedUpCallback: (OrderViewEntity) -> Unit,
     deleteOrderCallback: (OrderViewEntity) -> Unit,
-) = adapterDelegate<OrderViewEntity, ViewEntity>(R.layout.admin_order_item) {
+) = adapterDelegate<OrderViewEntity, ViewEntity>(R.layout.admin_order_item_compose) {
 
-    val binding = AdminOrderItemBinding.bind(itemView)
+    val binding = AdminOrderItemComposeBinding.bind(itemView)
+    binding.root.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 
     bind {
-        binding.email.text = item.order.email
-        binding.orderTitle.text = item.order.title
-        binding.orderTime.text = item.order.slotTime
-        when (item.order.status) {
-            Status.Ordered -> {
-                binding.orderReadyButton.text = "READY"
-                binding.orderReadyButton.setOnClickListener {
-                    onOrderReadyCallback(item)
+        binding.root.setContent {
+            MaterialTheme {
+                Row {
+                    Column {
+                        Text(item.order.email)
+                        Text(item.order.title)
+                        item.order.slotTime?.let { Text(it) }
+                    }
+                    when (item.order.status) {
+                        Status.Ordered -> {
+                            Button(onClick = { onOrderReadyCallback(item) }) {
+                                Text("READY")
+                            }
+                        }
+                        Status.Ready -> {
+                            Button(onClick = { onOrderPickedUpCallback(item) }) {
+                                Text("PICKED UP")
+                            }
+                        }
+                        Status.Picked -> {
+                            Button(onClick = { deleteOrderCallback(item) }) {
+                                Text("DELETE")
+                            }
+                        }
+                    }
+
                 }
             }
-            Status.Ready -> {
-                binding.orderReadyButton.text = "PICKED UP"
-                binding.orderReadyButton.setOnClickListener {
-                    onOrderPickedUpCallback(item)
-                }
-            }
-            Status.Picked -> {
-                binding.orderReadyButton.setOnClickListener(null)
-            }
-        }
-        binding.orderReadyButton.isVisible = item.order.status != Status.Picked
-        binding.orderDelete.isVisible = item.order.status == Status.Picked
-        binding.orderDelete.setOnClickListener {
-            deleteOrderCallback(item)
         }
     }
 }
